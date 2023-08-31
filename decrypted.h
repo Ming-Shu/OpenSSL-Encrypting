93038e080f830597591d699898e7641ebb69469b
Ming-Shu Wu <f16cdf16cd@gmail.com>
2023-08-31 17:02:52 +0800

    Update and rename decrypt.h to decrypted.h

--- decrypted.h
+++ decrypted.h
@@ -1,4 +1,4 @@
-
+#define BUFFER_SIZE 4096
 int generates_key_iv(unsigned char* key,unsigned char *iv,const unsigned char *password);
 int decryptFile(const char *inputFile, const char *outputFile);
 
